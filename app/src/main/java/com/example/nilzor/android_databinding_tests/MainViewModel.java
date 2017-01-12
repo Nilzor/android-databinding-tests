5321761ed6bb0f1c8781e79b6b354b33f96c42aa
Frode Nilsen <frode.nilsen@vg.no>
2017-01-12 09:11:41 +0100

    Simple databinding example where VM is recreated upon device rotation

---
+++ app/src/main/java/com/example/nilzor/android_databinding_tests/MainViewModel.java
@@ -0,0 +1,9 @@
+package com.example.nilzor.android_databinding_tests;
+
+
+import android.databinding.ObservableField;
+
+public class MainViewModel
+{
+    public ObservableField<String> Text = new ObservableField<>("Test");
+}
